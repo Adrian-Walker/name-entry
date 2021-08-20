6048ee9d91f200c46336ccd9a1987ad15ae4fe87
Adrian Walker <awalker3365@gmail.com>
2021-08-20 12:22:48 -0400

    8-20-21 1222p

--- Names.js
+++ Names.js
@@ -10,4 +10,22 @@ class Names extends Component {
         }
     }
 
+    render() {
+        return (
+            <div>
+                <input></input>
+                <h1></h1>
+                <ol>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ol>
+                <button onClick>Add</button>
+            </div>
+        )
+    }
+
 }
+
+export default Names
